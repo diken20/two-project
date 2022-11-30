2cc2a49df20ae9f20a0a701d6c189f5886d6eec4
Diken <diken.thapa@gmail.com>
2022-12-01 00:14:04 +0545

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import Button from "./Button";
+
+
+function App(){
+    return <div>
+        <h1 >Usefull website</h1>
+        <h3>which website do you want to open</h3>
+       <Button name="google"link="https://google.com"/>
+       
+       <Button name="facebook" link="https://facebook.com"/>
+       
+       <Button name="youtube" link="https://youtube.com"/><br></br>       
+       <Button name="ipo result"link= "https://iporesult.cdsc.com.np"/>
+       <Button name="github"link="https://github.com/"/>
+       
+
+
+    </div>
+
+} 
+export default App;
---
+++ src/Button.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+function Button({name,link}){
+    return <a href={link} target="_blank2" ><button className="button">{name}</button></a>
+}
+export default Button
